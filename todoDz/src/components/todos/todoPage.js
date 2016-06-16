eed06bb3c227a3d8ffc5eda32e491c1395b3e564
Ozzi <osborn@scitools.com>
2016-06-16 14:42:16 -0600

    adding  api framework to base functionality

--- todoDz/src/components/todos/todoPage.js
+++ todoDz/src/components/todos/todoPage.js
@@ -2,12 +2,64 @@
 
 var React = require('react');
 
+var TodoAPI = require("../../mockAPI/todoAPI");
+
+
 
 var Todos = React.createClass({
+
+    getInitialState: function () {
+        return {
+            todos: []
+        }
+
+    },
+
+    componentWillMount: function () {
+        this.setState({
+            todos: todoAPI.getAllTodos()
+        });
+
+    },
+
+
+
     render: function () {
+        var createTodoRow = function (todo) {
+
+            return(
+
+                <tr key={todo.id}>
+                    <td>{todo.id}</td>
+                    <td>{todo.title}</td>
+                    <td>{todo.description}</td>
+                </tr>
+
+            );
+
+
+
+        };
         return (
             <div>
                 <h2>Things we need todo</h2>
+                <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Desc</th>
+                    </tr>
+                </thead>
+
+                    <tbody>
+
+                    {this.state.todos.map(createTodoRow, this)}
+
+                    </tbody>
+
+
+                </table>
             </div>
         );
     }
